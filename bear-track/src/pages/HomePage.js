c357f0a609b06d99952f4f4a44996e50e762dc5e
Joseph <jgrados@ggc.edu>
2023-11-14 22:23:14 -0500

    Big calendar implementation

--- bear-track/src/pages/HomePage.js
+++ bear-track/src/pages/HomePage.js
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom';
 import BellIcon from '../components/bell-filled.svg';
 import 'firebase/compat/firestore';
 import { useUser } from './UserContext';
+import { Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 
 
@@ -20,7 +23,19 @@ const HomePage = () => {
     console.log("Printing from successful image addition: ",)
   }
  
-  console.log(firebase.auth().currentUser.uid)
+  console.log(firebase.auth().currentUser.uid);
+
+
+  const events = [
+    {
+      title: 'Team Meeting',
+      start: new Date(),
+      end: new Date(),
+    },
+    // Add more events as needed
+  ];
+
+  const localizer = momentLocalizer(moment);
  
   return (
     <div className="homepage">
@@ -41,7 +56,17 @@ const HomePage = () => {
 
         <div className='profileName' >{user.userName}</div>
 
-          
+
+        <div style={{ height: '600px', margin: '20px', width: '500px'}}>
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: '100%' ,width: '100%',position: 'relative', top : '100px', left: '-500px'}}
+          />
+
+        </div>
       
         <Link to="/">
         <button className="logout-button">Logout</button>
